66c84f0780b31a9856fb588f399a44fef30be264
Yashwant <yashwantsingh.sngh@gmail.com>
2019-08-09 19:12:00 +0530

    Add

--- go/doc.go
+++ go/doc.go
@@ -1,3 +1,9 @@
-//mlpack is a fast, flexible machine learning library, written in C++, that aims to provide fast, extensible implementations of cutting-edge machine learning algorithms. 
-//mlpack provides these algorithms as simple command-line programs, Python bindings, and C++ classes which can then be integrated into larger-scale machine learning solutions.
+/*
+
+mlpack is a fast, flexible machine learning library, written in C++, that aims to provide fast, extensible implementations of cutting-edge machine learning algorithms. 
+mlpack provides these algorithms as simple command-line programs, Go bindings, and C++ classes which can then be integrated into larger-scale machine learning solutions.
+You may also need to set the environment variable LD_LIBRARY_PATH on most Linux systems.
+
+export LD_LIBRARY_PATH=/path/to/go/src/Yashwants19/v1/go
+*/
 package mlpack //import "github.com/Yashwants19/v1/go"
