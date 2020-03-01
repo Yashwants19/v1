a6e58741de056ae8e063e5af1aa22c0eb792f3c2
Yashwant <yashwantsingh.sngh@gmail.com>
2020-03-01 11:14:32 +0530

    Add go doc

--- mlpack/doc.go
+++ mlpack/doc.go
@@ -4,4 +4,4 @@
 // programs, Go bindings, and C++ classes which can then be integrated into 
 // larger-scale machine learning solutions.
 
-package mlpack // import "mlpack.org/v1"
+package mlpack // import "github.com/Yashwants19/v1/mlpack"
